af2c9f53cf2320cec1bc142531de88f3d23bc2eb
Mut Ze <mutongsoft@gmail.com>
2023-12-29 17:26:55 +0800

    Initialize vanilla react app

---
+++ build.js
@@ -0,0 +1,15 @@
+const esbuild = require("esbuild");
+
+async function build() { 
+  await esbuild.build({
+    entryPoints: ["./src/index.html", "./src/_app.tsx"],
+    bundle: true,
+    outdir: "dist",
+    platform: "browser",
+    loader: {".html": "copy"},
+    format: "iife",
+    sourcemap: true,
+  });
+}
+
+build()
