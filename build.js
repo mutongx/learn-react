bf75278771e5303da3d9da0c0a58d554fdbdfdd6
Mut Ze <mutongsoft@gmail.com>
2023-12-29 18:04:54 +0800

    Add CSS

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const esbuild = require("esbuild");
 
 async function build() { 
   await esbuild.build({
-    entryPoints: ["./src/index.html", "./src/_app.tsx"],
+    entryPoints: ["./src/index.html", "./src/_app.css", "./src/_app.tsx"],
     bundle: true,
     outdir: "dist",
     platform: "browser",
